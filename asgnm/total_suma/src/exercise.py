4b23a0ca8ccc70de070bc9adb55adfce15cee8e5
AlejandroL12 <A01740869@itesm.mx>
2021-09-21 14:27:33 +0000

    contador_nums

---
+++ asgnm/total_suma/src/exercise.py
@@ -0,0 +1,15 @@
+
+def main():
+    s=0
+    c=0
+    while s<1000:
+        n=int(input())
+        c=c+1
+        s=s+n
+
+    print(f"suma={s}")
+    print(f"cantidad de números={c}")
+
+
+if __name__ == '__main__':
+    main()
